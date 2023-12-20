8ffeb36efa1681735755566b026f299c59f89f01
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-12-19 19:49:34 -0500

    remove old code. adjus test

--- tests/video-hunt.spec.ts
+++ tests/video-hunt.spec.ts
@@ -6,32 +6,29 @@ dotenv.config();
 
 const options: GenerateOptions = {
   projectDir: "./tests/temp",
-  regenerateDescription: true,
+  regenerateDescription: false,
   technology: "next14",
   model: "gpt-4-1106-preview",
 };
 
 const instructions: string[] = [
-  // "Initialize a new Next.js project with TypeScript support.",
-  // "Set up Prisma and create a Prisma schema for your database.",
   "Add a 'Video' model to the Prisma schema with fields like 'id', 'title', 'url', 'createdAt', and 'votes'.",
-  // "Set up your database (e.g., PostgreSQL, MySQL) and run Prisma migrations.",
   "Create a page with a list of videos. Fetch video data from the database and display it.",
   "Add an API route in Next.js to handle video submissions.",
   "Create a form on the front-end to submit new videos. This form should make a POST request to your submission API route.",
   "Add an API route for upvoting a video. This route should update the 'votes' field of a video in the database.",
   "Implement a button or a mechanism on the video list page to upvote videos, making a request to your upvote API route.",
-  // "Implement user authentication (e.g., with NextAuth.js) to allow only authenticated users to submit and upvote videos.",
-  // "Style the application using your preferred CSS solution (e.g., Tailwind CSS, Styled Components).",
-  // "Add error handling and validation both on the client-side (form submission) and server-side (API routes).",
-  // "Write unit and integration tests for your API routes and components.",
-  // "Deploy your application on a platform like Vercel or Netlify.",
 ];
 
 test.only("VideoHunt test", async ({ page, request }) => {
-  test.setTimeout(300000);
+  test.setTimeout(1200000);
   const actionlist: ActionList = [];
 
+  // // one big instruction:
+  // const actions = await generate(instructions.join("\n"), options);
+  // actionlist.push(...actions);
+
+  // break it down into smaller parts
   for (const instruction of instructions) {
     const actions = await generate(instruction, options);
     actionlist.push(...actions);
